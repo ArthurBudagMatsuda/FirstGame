00e87272730e591aeda5b8b14b362bbc1b6f83c5
ArthurMatsuda <Arthurmatsudaoficial@gmail.com>
2024-01-26 15:56:11 -0300

    Added Player movement (no fps Limit)
    -add player movement
    -add player spritesheets

--- Origem.cpp
+++ Origem.cpp
@@ -37,29 +37,68 @@ int main() {
 	//settings.antialiasingLevel = 8;
 	//sf::RenderWindow window(sf::VideoMode(800, 600), "RPG GAME",sf::Style::Default, settings);
 	sf::RenderWindow window(sf::VideoMode(800, 600), "RPG GAME");
-	sf::CircleShape shape(50.f);
-	sf::RectangleShape rectangle(sf::Vector2f(150, 50));
-	sf::CircleShape square(80.f, 3);
-	rectangle.setOrigin(sf::Vector2f(0, 0));
-	//rectangle.setOrigin(rectangle.getSize()/ 2.0f); garante que sempre a origin será o centro
-	square.setPosition(sf::Vector2f(50,50));
-	square.setFillColor(sf::Color::Black);
-	rectangle.setPosition(sf::Vector2f(100, 100));
-	shape.setFillColor(sf::Color::Magenta);
-	shape.setPosition(sf::Vector2f(100, 100));
+
+	///------------------LOAD-----------------
+	sf::Texture PlayerTexture;
+	sf::Sprite playerSprite;
+
+	if (PlayerTexture.loadFromFile("assets/player/texture/SpriteSheets.png")) {//fazer uma classe load game para dar load em tudo 
+		std::cout << "Player Texture Loaded.";
+		playerSprite.setTexture(PlayerTexture);
+
+		int Xindex =2;//120
+		int yindex = 2;//100
+		//X,Y,width,height
+		playerSprite.setTextureRect(sf::IntRect(Xindex * 120, yindex * 100,120,100));
+		playerSprite.scale(sf::Vector2f(2, 2));
+	}
+	else {
+		std::cout << "Player Texture Failed.";
+	}
+
+	///------------------LOAD-----------------
+	//sf::CircleShape shape(50.f);
+	//sf::RectangleShape rectangle(sf::Vector2f(150, 50));
+	//sf::CircleShape square(80.f, 3);
+	//rectangle.setOrigin(sf::Vector2f(0, 0));
+	////rectangle.setOrigin(rectangle.getSize()/ 2.0f); garante que sempre a origin será o centro
+	//square.setPosition(sf::Vector2f(50,50));
+	//square.setFillColor(sf::Color::Black);
+	//rectangle.setPosition(sf::Vector2f(100, 100));
+	//shape.setFillColor(sf::Color::Magenta);
+	//shape.setPosition(sf::Vector2f(100, 100));
 
 	while (window.isOpen()) {
-
+		sf::Vector2f postion = playerSprite.getPosition();
 
 		// -------------- UPDATE ---------------- // 
 		sf::Event event; // criando um objeto chamado event
 		while (window.pollEvent(event))// looping na fila de eventos (queue fila em ingles) 
 		{
 			// se o tipo for close fechar
-			if (event.type == sf::Event::Closed) 
+			if (event.type == sf::Event::Closed) {
 				window.close();
 
+			}
 
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+
+				playerSprite.setPosition(postion + sf::Vector2f(1, 0));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+
+
+				playerSprite.setPosition(postion - sf::Vector2f(1, 0));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+
+
+				playerSprite.setPosition(postion - sf::Vector2f(0, 1));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+
+				playerSprite.setPosition(postion + sf::Vector2f(0, 1));
+			}
 			// etc ...
 		}
 		// -------------- UPDATE ---------------- // 
@@ -70,10 +109,11 @@ int main() {
 		// Activate the window for OpenGL rendering
 		window.setActive();
 		window.clear(sf::Color::Blue);//limpando  a screen
-		window.draw(shape);
-		window.draw(rectangle);
-		window.draw(square);
-		//draw
+		////draw
+		//window.draw(shape);
+		//window.draw(rectangle);
+		//window.draw(square);
+		window.draw(playerSprite);//nao pode dar draw em uma texture
 		//draw player
 		window.display(); // chamando a date q tava no back-bufffer e botando na screenb
 		// -------------- DRAW ---------------- // 
